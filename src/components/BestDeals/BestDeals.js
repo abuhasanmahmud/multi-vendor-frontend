14f98dfde3e5186bad6cc2d305bcffcdff66cc05
abuhasanmahmud <mahmud.me420@gmail.com>
2023-07-20 23:15:05 +0600

    Fat(v-1) sign up page done

--- src/components/BestDeals/BestDeals.js
+++ src/components/BestDeals/BestDeals.js
@@ -5,6 +5,7 @@ import ProductCard from "../src/ProductCard/ProductCard";
 
 const BestDeals = () => {
   const [data, setData] = useState([]);
+  console.log("ss");
   const { allProducts } = useSelector((state) => state.products);
   useEffect(() => {
     const allProductsData = allProducts ? [...allProducts] : [];
